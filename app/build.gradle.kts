e0abf225f789c992f1d2174f4b0b1a2fec2e22b9
Kalpesh Kulye <kulyekalpesh@gmail.com>
2025-09-13 00:42:03 +0530

    updated target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.assignment"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
